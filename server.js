4d42897b9a6dbed4564325c779940c9fc57a19ba
David <davidcarmel@Davids-MacBook-Pro.local>
2018-10-12 18:12:08 -0400

    fix server error

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use('/api/posts', posts);
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req,res) => {
-    res.senfFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
