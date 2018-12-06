6baa2910fbc93c38510f7b407c7e823db350798a
David <davidcarmel@Davids-MacBook-Pro.local>
2018-12-05 20:16:43 -0500

    patch merge npm package

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,9 +4,9 @@ const request = require('supertest');
 const {app} = require('./../server');
 const User = require('./../models/User');
 
-// beforeEach((done) => {
-//   User.remove({}).then(() => done());
-// });
+beforeEach((done) => {
+  User.remove({}).then(() => done());
+});
 
 
 describe('POST /api/users/register', () => {
